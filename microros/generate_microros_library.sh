0f047c3346f347060dfc63d36763942c79ce5aed
Antón Casas <antoncasas@eprosima.com>
2025-04-02 15:45:30 +0200

    Ignore ament_cmake_ros/rmw_test_fixture_implementation
    Signed-off-by: Antón Casas <antoncasas@eprosima.com>

--- microros/generate_microros_library.sh
+++ microros/generate_microros_library.sh
@@ -38,6 +38,7 @@ pushd $INSTALL_DIR/micro_ros_dev > /dev/null
 	git clone -b rolling https://github.com/ament/googletest src/googletest;
 	git clone -b rolling https://github.com/ros2/ament_cmake_ros src/ament_cmake_ros;
 	git clone -b rolling https://github.com/ament/ament_index src/ament_index;
+    touch src/ament_cmake_ros/rmw_test_fixture_implementation/COLCON_IGNORE;
     colcon build --cmake-args -DBUILD_TESTING=OFF;
 popd > /dev/null
 
